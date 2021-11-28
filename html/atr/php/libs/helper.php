c4e524e465c8f035d6e1cd4c78d325eea8cf2755
NAOKI_YANAGAWA <naoki.yanagawa0601@gmail.com>
2021-11-28 13:53:20 +0900

    不正なパラメーターでないことを確認する機能を追加（staticListの値）

--- html/atr/php/libs/helper.php
+++ html/atr/php/libs/helper.php
@@ -1,22 +1,20 @@
 <?php
+
+use staticList;
+
 function get_param($key, $default_val, $is_post = true)
 {
-
     $arry = $is_post ? $_POST : $_GET;
     return $arry[$key] ?? $default_val;
 }
 
 function redirect($path)
 {
-
     if ($path === GO_HOME) {
-
         $path = get_url('');
-    } else if ($path === GO_REFERER) {
-
+    } elseif ($path === GO_REFERER) {
         $path = $_SERVER['HTTP_REFERER'];
     } else {
-
         $path = get_url($path);
     }
 
@@ -32,35 +30,42 @@ function the_url($path)
 
 function get_url($path)
 {
-
     return BASE_CONTEXT_PATH . trim($path, '/');
 }
 
 function is_alnum($val)
 {
-
     return preg_match("/^[a-zA-Z0-9]+$/", $val);
 }
 
 function escape($data)
 {
     if (is_array($data)) {
-
         foreach ($data as $prop => $val) {
             $data[$prop] = escape($val);
         }
 
         return $data;
     } elseif (is_object($data)) {
-
         foreach ($data as $prop => $val) {
             $data->$prop = escape($val);
         }
 
         return $data;
     } else {
-
         return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-
     }
+}
+
+function is_staticListParam($val, $staic_list_param)
+{
+    $res = false;
+
+    foreach ($staic_list_param as $key => $value) {
+        if ($key === $val) {
+            $res = true;
+        }
+    }
+
+    return $res;
 }
